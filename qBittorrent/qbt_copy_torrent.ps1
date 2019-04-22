7d7599b7d01a1ebe0625e88e1bc22d8380f04977
kysdm <gxk7231@gmail.com>
2019-04-22 20:15:40 +0800

    fix

--- qBittorrent/qbt_copy_torrent.ps1
+++ qBittorrent/qbt_copy_torrent.ps1
@@ -1,31 +1,43 @@
 # 自动复制种子文件到下载文件存放位置
-# v0.1
-#
-# 第一次用PS写脚本，因为没有BUG吧
+# v0.3
 #
 # 【用法】
-# 在qbt的完成时运行外部程序中加入下面的内容 【C:\Users\kysdm\Desktop\1.ps1】为PS脚本所在绝对路径
-# PowerShell.exe -file C:\Users\kysdm\Desktop\1.ps1 -N "%N" -L "%L" -G "%G" -F "%F" -R "%R" -D "%D" -C "%C" -Z "%Z" -T "%T" -I "%I"
-# 还需修改第28行的代码
+# 在qbt的完成时运行外部程序中加入下面的内容 【C:\Plugin\qbittorrent_x64_portable\qbt_copy_torrent.ps1】为PS脚本所在绝对路径
+# PowerShell.exe -file "C:\Plugin\qbittorrent_x64_portable\qbt_copy_torrent.ps1" -N "%N" -L "%L" -G "%G" -F "%F" -R "%R" -D "%D" -C "%C" -Z "%Z" -T "%T" -I "%I"
+# 还需修改第12,13行的代码
 #
-# 接下来需要解决的问题
-# 如果种子没有文件夹，复制过去的种子就肯定是与文件在同级目录的，但如果种子是带文件夹的，现阶段只能复制到上级目录，可能解决起来很简单，没时间看了-咕咕咕-
-#
-# 获取外部变量
-param($N,$L,$G,$F,$R,$D,$C,$Z,$T,$I)
-# 调试部分
-# write-output "Torrent名称：$N"   | out-file -filepath   d:\debug.txt 
-# write-output "分类：$L"  | out-file  -Append d:\debug.txt 
-# write-output "标签（用逗号分隔）：$G"  | out-file  -Append  d:\debug.txt 
-# write-output "内容路径（与多文件 torrent 的根目录相同）：$F"  | out-file  -Append d:\debug.txt 
-# write-output "根目录（第一个 torrent 的子目录路径）：$R"  | out-file  -Append d:\debug.txt 
-# write-output "保存路径：$D"  | out-file  -Append  d:\debug.txt 
-# write-output "文件数：$C"  | out-file  -Append d:\debug.txt 
-# write-output "Torrent 大小：$Z"  | out-file  -Append d:\debug.txt 
-# write-output "当前 tracker：$T"  | out-file  -Append  d:\debug.txt 
-# write-output "哈希值：$I"  | out-file  -Append  d:\debug.txt 
+param($N,$L,$G,$F,$R,$D,$C,$Z,$T,$I) # 获取外部变量
 
-# 定义qbt存放种子文件的目录
-$BT_backup = "C:\Users\kysdm\Desktop\qbittorrent_x64_portable\profile\qBittorrent\data\BT_backup"
+$debug_switch = "1" # 0不输出log，1输出log
+$BT_backup = "C:\Plugin\qbittorrent_x64_portable\profile\qBittorrent\data\BT_backup" # 定义qbt存放种子文件的目录
+$log_folder = "C:\Plugin\qbittorrent_x64_portable\log" # 定义脚本debug日志文件存放位置
 
-copy-Item "$BT_backup\$I.torrent"  "$D\$N.torrent"
+If( $debug_switch -eq 1 )
+  { 
+   switch( Test-Path $log_folder )   {      "False"  { New-Item $log_folder -type directory }    } # 判断Log文件夹是否存在
+   $time = Get-Date -Format 'yyyyMMddHHmmss'  #获取当前时间
+   $outlog = -join ("$log_folder","\","$time","_","$I",".log")
+   write-output "Torrent名称：$N"   | out-file -filepath  $outlog
+   write-output "分类：$L"  | out-file  -Append $outlog
+   write-output "标签（用逗号分隔）：$G"  | out-file  -Append  $outlog
+   write-output "内容路径（与多文件 torrent 的根目录相同）：$F"  | out-file  -Append $outlog 
+   write-output "根目录（第一个 torrent 的子目录路径）：$R"  | out-file  -Append $outlog
+   write-output "保存路径：$D"  | out-file  -Append  $outlog
+   write-output "文件数：$C"  | out-file  -Append $outlog 
+   write-output "Torrent 大小：$Z"  | out-file  -Append $outlog 
+   write-output "当前 tracker：$T"  | out-file  -Append  $outlog 
+   write-output "哈希值：$I"  | out-file  -Append  $outlog 
+  }
+
+$shortI=$I.Substring(0,7)
+  
+If( ($C -eq 1) -and ("$F" -eq "$R") )
+  {
+   $outname = -join ("$D","\","$N","_","$time",".torrent")  
+  }
+Else
+  {
+   $outname = -join ("$R","\","$N","_","$time",".torrent")  
+  }
+ 
+copy-Item "$BT_backup\$I.torrent"  "$outname" 
